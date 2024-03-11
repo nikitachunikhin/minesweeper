71d134543cd8baafbac9a9bbca010b4c7e2a8a64
Michiel Florian de Jong <michiel.dejong@student.kuleuven.be>
2024-03-11 14:29:51 +0100

    Create Field class

--- src/Model/Field.java
+++ src/Model/Field.java
@@ -1,4 +1,42 @@
 package Model;
 
 public class Field {
+    int height;
+    int width;
+    int numberOfMines;
+    int numberOfOpendCells = 0;
+    GameState state;
+
+
+    //Constructor
+    public Field(int height, int width, int numberOfMines){
+        this.height = height;
+        this.width = width;
+        this.numberOfMines = numberOfMines;
+        state = GameState.RUNNING;
+
+    }
+
+    public Tile[][] getField(){
+
+
+    }
+
+    public void changeGameStatus(Gamestate newState){
+        state = GameState.newState;
+    }
+
+    public Tile[][] getTile(int x, int y){
+
+    }
+
+    public int[][] generateMines(int numberOfMines){
+
+    }
+
+    public Tile[][] getNeighbour(int x, int y){
+
+    }
+
+
 }
